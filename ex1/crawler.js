949b73244dcc8f5a2a1a8ab0c79d1f372055a1ca
charlenelecouturier <33778377+charlenelecouturier@users.noreply.github.com>
2019-09-07 15:12:29 -0400

    modif :crawler.js- on separe les 2 noms

--- ex1/crawler.js
+++ ex1/crawler.js
@@ -3,20 +3,21 @@ var cheerio = require('cheerio');
 
 var url = "http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=";
 
-let req=0;
-for(let i=1;i<1601;i++){
+//for(let i=1;i<1601;i++){
     JSONobject= new Object();
-   
-    request(url+i, function (error, response, body) {
-        req++;
+    request(url+'1281', function (error, response, body) {
     if (!error) {
         var $ = cheerio.load(body);
-        var title = $('.heading').text();
+        var name = $('.heading').map(function(i, el) {
+            // this === el
+            return $(this).text();
+          }).get();
 
-        console.log("spell "+i+": " +title) ;
+     //  console.log("spell "+i+": " +name) ;
+     console.log("spell : " +name) ;
     }
     else {
         console.log("We’ve encountered an error: " + error);
     }
     });
-}
+//}
