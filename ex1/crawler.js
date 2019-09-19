3c0345d3ba7dc4639345c0a1a167293a96a3c682
charlenelecouturier <33778377+charlenelecouturier@users.noreply.github.com>
2019-09-19 17:34:53 -0400

    add : fonction d'insertion

--- ex1/crawler.js
+++ ex1/crawler.js
@@ -2,114 +2,105 @@ var request = require('request');
 var cheerio = require('cheerio');
 var MongoClient = require('mongodb').MongoClient;
 
-var url = "http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=";
-//scrolling de toute les pages de sort
 var JSONobject = new Object;
 
-function insertSpell(doc){
+//fonction d'insertion des JSON dans la base de donnees
+function insertSpell(doc) {
     const urlM = 'mongodb://localhost:27017';
 
     MongoClient.connect(urlM, { useNewUrlParser: true }, (err, client) => {
-    
-      
-    if (err) throw err;
-
-    const db = client.db("DB");
 
 
-    db.collection('spells').insertOne(doc).then((doc) => {
+        if (err) throw err;
 
-        console.log('inserted')
-      
-    }).catch((err) => {
+        const db = client.db("DB");
 
-        console.log(err);
-    }).finally(() => {
 
-        client.close();
+        db.collection('spells').insertOne(doc).then((doc) => {
+
+            console.log('inserted')
+
+        }).catch((err) => {
+
+            console.log(err);
+        }).finally(() => {
+
+            client.close();
+        });
     });
-});
 }
-function crawl(){
 
-for (let i = 1; i < 1601; i++) {
-    request(url + i, function (error, response, body) {
-        if (!error) {
-            var $ = cheerio.load(body);
-            //nom du sort
-            var name = $('.heading').map(function (i, el) {
-                // this === el
-                return $(this).text();
-            }).get().join(', ');
-            console.log(i);
-            var spellCarac = $('.SpellDiv p.SPDet').map(function (i, el) {
-                // this === el
-                return $(this).html();
-            }).get().join('\n');
-           // console.log(spellCarac);
-            var level = spellCarac.match(/\<b\>Level\<\/b\>.*/g);
-            var levelint=null;
-            if (level){
-                level =level[0].slice(13,);
-                var wizard= level.match(/wizard.*/g);
-                if(wizard){
-                    level=wizard.toString().match(/[0-9]/g)[0]
+
+
+//scrolling de toutes les pages de sort
+function crawl() {
+
+    //url du site a scroller
+    var url = "http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=";
+
+    //on parcourt les pages entre 1 et 1600 
+    for (let i = 1; i < 1601; i++) {
+        request(url + i, function (error, response, body) {
+            if (!error) {
+                var $ = cheerio.load(body); //on recupere le body de la page html
+
+
+                //nom du sort
+                var name = $('.heading').map(function (i, el) {
+                    // this === el
+                    return $(this).text();
+                }).get().join(', ');
+                console.log(i);
+                var spellCarac = $('.SpellDiv p.SPDet').map(function (i, el) {
+                    // this === el
+                    return $(this).html();
+                }).get().join('\n');
+                var level = spellCarac.match(/\<b\>Level\<\/b\>.*/g);
+                var levelint = null;
+                if (level) {
+                    level = level[0].slice(13);
+                    var wizard = level.match(/wizard.*/g);
+                    if (wizard) { //on prend le level du wizard s,il existe
+                        level = wizard.toString().match(/[0-9]/g)[0]
+                    }
+                    else {//sinon on prend n'importe quel level
+                        level = level.match(/[0-9]/g)[0];
+                    }
+                    levelint = parseInt(level); //on veut l'inserer en tant qu'integer dans la bdd
+
+
+                //on récupère seulement les lettres des components : V,S,M,F,DF
+                var components = spellCarac.match(/\<b\>Components\<\/b\>.*/g)
+                if (components) {
+                    components = components[0].slice(18).match(/(M\/DF)|(F\/DF)|[VSMF]|(DF)/g);
                 }
-               else{
-    level=level.match(/[0-9]/g)[0];
-               }
-               levelint=parseInt(level);
-            }
 
+                var spellRes = spellCarac.match(/\<b\>Spell Resistance\<\/b\>.*/g);
+                //in some case Spell res is not defined, 
+                if (spellRes) {
+                    spellRes = spellRes[0].slice(24).match(/(yes)|(no)|(none)/gi); //match yes/no/none en n'étant pas sensible a la casse
+                }
+                else {
+                    spellRes = false;
+                }
 
-           
-            //on récupère seulement les lettres des components : V,S,M,F,DF
-            var components = spellCarac.match(/\<b\>Components\<\/b\>.*/g)
-            if (components){
-               components =components[0].slice(18).match(/(M\/DF)|(F\/DF)|[VSMF]|(DF)/g);
-            }
-           
-            var spellRes= spellCarac.match(/\<b\>Spell Resistance\<\/b\>.*/g);
-            //in some case Spell res is not defined, 
-            if (spellRes) {
-                spellRes = spellRes[0].slice(24).match(/(yes)|(no)|(none)/gi); //match yes/no/none en n'étant pas sensible a la casse
+                //creation de l'objet Json
+                JSONobject = {
+                    name: name,
+                    level: levelint,
+                    components: components,
+                    spell_resistance: spellRes
+                };
+                //insertion dans la base de donnees
+                insertSpell(JSONobject);
             }
             else {
-                spellRes = false;
+                console.log("We’ve encountered an error: " + error);
             }
-           //console.log("spell : " + name);
-           // console.log("level : " + level);
-          // console.log("components : " + components);
-          // console.log("spell resistance : " + spellRes);
-            //creation de l'objet Json
-            JSONobject = {
-                name: name,
-            level: levelint,
-                components: components,
-                spell_resistance: spellRes
-            };
+        });
 
-       insertSpell(JSONobject);
-        }
-        else {
-            console.log("We’ve encountered an error: " + error);
-        }
-    });
+    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
 
 crawl();
