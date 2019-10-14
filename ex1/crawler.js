8314cc40671851b048d579d04ce10e7787ae1406
charlenelecouturier <33778377+charlenelecouturier@users.noreply.github.com>
2019-10-13 20:18:06 -0400

    FEATURE  : ex1 final commit

--- ex1/crawler.js
+++ ex1/crawler.js
@@ -2,121 +2,107 @@ var request = require('request');
 var cheerio = require('cheerio');
 var insert = require('./insertDB.js');
 var JSONobject = new Object;
-
-var tableObjectMongo = new Array;
-var tableObjectSQL = [];
-
 //scrolling de toutes les pages de sort
-  function crawl() {
-    var promise = new Promise(function(resolve,reject){
-        //url du site a scroller
-        var url = "http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=";
+function crawl() {
+    
+    //url du site a scroller
+    var url = "http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=";
+
     //on parcourt les pages entre 1 et 1600 
-        for (let i = 1; i < 1601; i++) {
-            request(url + i, function (error, response, body) {
+    for (let i = 1; i < 1601; i++) {
+        request(url + i, function (error, response, body) {
+            if (!error) {
+                var $ = cheerio.load(body); //on recupere le body de la page html
+
+                //nom du sort
+                var name = $('.heading').map(function (i, el) {
+                    // this === el
+                    return $(this).text();
+                }).get().join(', ');
+                console.log(i);
+                var spellCarac = $('.SpellDiv p.SPDet').map(function (i, el) {
+                    // this === el
+                    return $(this).html();
+                }).get().join('\n');
+
+                //level du sort
+                var level = spellCarac.match(/\<b\>Level\<\/b\>.*/g);
+                //classe de personnage pouvant faire le sort
+                var dndClass=null; 
+                var dndClassSQL=null; // en sql il faut que ce soit un String et non un tableau
+                var levelint = null;
+                if (level) {
+                    level = level[0].slice(13);
+                    dndClass=level;
+                    dndClass = dndClass.slice(0,-1);
+                    dndClass = dndClass.trim().split(/\s[0-9],*\s*/g); //on enleve les chiffres des levels pour ne garder que les classes (wizard etc..)
+                    dndClassSQL= dndClass.toString();
+                    var wizard = level.match(/wizard.*/g);
+                    if (wizard) { //on prend le level du wizard s,il existe
+                        level = wizard.toString().match(/[0-9]/g)[0];
+                    }
+                    else {//sinon on prend n'importe quel level
+                        level = level.match(/[0-9]/g)[0];
+                    }
+                    levelint = parseInt(level); //on veut l'inserer en tant qu'integer dans la bdd
+                }
                 
-                if (!error) {
-                    var $ = cheerio.load(body); //on recupere le body de la page html
-    
-                    //nom du sort
-                    var name = $('.heading').map(function (i, el) {
-                        // this === el
-                        return $(this).text();
-                    }).get().join(', ');
-                    var spellCarac = $('.SpellDiv p.SPDet').map(function (i, el) {
-                        // this === el
-                        return $(this).html();
-                    }).get().join('\n');
-    
-                    //level du sort
-                    var level = spellCarac.match(/\<b\>Level\<\/b\>.*/g);
-                    //classe de personnage pouvant faire le sort
-                    var dndClass=null; 
-                    var dndClassSQL=null; // en sql il faut que ce soit un String et non un tableau
-                    var levelint = null;
-                    if (level) {
-                        level = level[0].slice(13);
-                        dndClass=level;
-                        dndClass = dndClass.slice(0,-1);
-                        dndClass = dndClass.trim().split(/\s[0-9],*\s*/g);
-                        dndClassSQL= dndClass.toString();
-                        var wizard = level.match(/wizard.*/g);
-                        if (wizard) { //on prend le level du wizard s,il existe
-                            level = wizard.toString().match(/[0-9]/g)[0];
-                        }
-                        else {//sinon on prend n'importe quel level
-                            level = level.match(/[0-9]/g)[0];
-                        }
-                        levelint = parseInt(level); //on veut l'inserer en tant qu'integer dans la bdd
-                    }
-                    
-                    //components du sort
-                    var components = spellCarac.match(/\<b\>Components\<\/b\>.*/g);
-                    var componentsSQL = null; // en sql il faut que ce soit un String et non un tableau
-    
-                    if (components) {
-                        components = components[0].slice(18).match(/(M\/DF)|(F\/DF)|[VSMF]|(DF)/g); //on récupère seulement les lettres des components : V,S,M,F,DF
-    
-                      
-                        if(components){
-                            componentsSQL=components.toString();
-                             }
-                    }
-    
-                    //spell resistance du sort
-                    var spellRes = spellCarac.match(/\<b\>Spell Resistance\<\/b\>.*/g);
-                    //in some case Spell res is not defined, 
-                    if (spellRes) {
-                        spellRes = spellRes[0].slice(24).match(/(yes)|(no)|(none)/gi); //match yes/no/none en n'étant pas sensible a la casse
-                        if(spellRes){
-                            spellRes=spellRes[0];
-                        }else{
-                            spellRes = false;
-                        }
-                    }
-                    else {
+                //components du sort
+                var components = spellCarac.match(/\<b\>Components\<\/b\>.*/g);
+                var componentsSQL = null; // en sql il faut que ce soit un String et non un tableau
+
+                if (components) {
+                    components = components[0].slice(18).match(/(M\/DF)|(F\/DF)|[VSMF]|(DF)/g); //on récupère seulement les lettres des components : V,S,M,F,DF
+
+                  
+                    if(components){
+                        componentsSQL=components.toString();
+                         }
+                }
+
+                //spell resistance du sort
+                var spellRes = spellCarac.match(/\<b\>Spell Resistance\<\/b\>.*/g);
+                //in some case Spell res is not defined, 
+                if (spellRes) {
+                    spellRes = spellRes[0].slice(24).match(/(yes)|(no)|(none)/gi); //match yes/no/none en n'étant pas sensible a la casse
+                    if(spellRes){
+                        spellRes=spellRes[0];
+                    }else{
                         spellRes = false;
                     }
-    
-                    //creation de l'objet Json
-                    JSONobject = {
-                        name: name,
-                        class: dndClass,
-                        level: levelint,
-                        components: components,
-                        spell_resistance: spellRes
-                    };
-    
-                       //objectSQL
-                       objectSQL = {
-                        name: name,
-                        class: dndClassSQL,
-                        level: levelint,
-                        components: componentsSQL,
-                        spell_resistance: spellRes
-                    };
-                    //insertion dans la base de donnees
-                    
-                    console.log(tableObjectMongo.push(JSONobject));
-                    tableObjectSQL.push(objectSQL);
-    
-                   /* insert.insertSpellMongo(JSONobject,"DB","spells");
-                   insert.insertSpellSQL(objectSQL); */
-    
                 }
                 else {
-                    console.log("We’ve encountered an error: " + error);
+                    spellRes = false;
                 }
-            });
-    
-        }
-        resolve();
-    });
-return promise;
+
+                //creation de l'objet Json
+                JSONobject = {
+                    name: name,
+                    class: dndClass,
+                    level: levelint,
+                    components: components,
+                    spell_resistance: spellRes
+                };
+
+                   //objectSQL
+                   objectSQL = {
+                    name: name,
+                    class: dndClassSQL,
+                    level: levelint,
+                    components: componentsSQL,
+                    spell_resistance: spellRes
+                };
+                //insertion dans la base de donnees
+               insert.insertSpellMongo(JSONobject);
+               insert.insertSpellSQL(objectSQL);
+
+            }
+            else {
+                console.log("We’ve encountered an error: " + error);
+            }
+        });
+
+    }
 }
-    
-
 insert.createTableSQL();
-crawl().then(insert.insertSpellMongo(tableObjectMongo,"DB","spells"));
-
-
+setTimeout(crawl,3000);
--- ex1/insertDB.js
+++ ex1/insertDB.js
@@ -4,32 +4,28 @@ var sqlite3 = require('sqlite3').verbose();
 //fonction d'insertion des JSON dans la base de donnees MongoDB
  
     
-    function insertSpellMongo(doc, dataBase, collection) {
-        var promise= new Promise(function(resolve, result){
-            const urlM = 'mongodb://localhost:27017';
+    function insertSpellMongo(doc) {
+    const urlM = 'mongodb://localhost:27017';
 
-            MongoClient.connect(urlM, { useUnifiedTopology: true, useNewUrlParser: true }, (err, client) => {
-    
-                if (err) throw err;
-    
-                const db = client.db(dataBase);
-    
-                //db.collection(collection).deleteMany();
-                db.collection(collection).insertOne(doc).then((doc) => {
-    
-                    console.log('inserted')
-    
-                }).catch((err) => {
-    
-                    console.log(err);
-                }).finally(() => {
-    
-                    client.close();
-                });
-            });
-            resolve();
-        }); 
-    return promise;
+    MongoClient.connect(urlM, { useUnifiedTopology: true, useNewUrlParser: true }, (err, client) => {
+
+        if (err) throw err;
+
+        const db = client.db("DB");
+
+
+        db.collection('spells').insertOne(doc).then((doc) => {
+
+            console.log('inserted Mongo')
+
+        }).catch((err) => {
+
+            console.log(err);
+        }).finally(() => {
+
+            client.close();
+        });
+    });
 }
 
 
@@ -38,21 +34,21 @@ function createTableSQL() {
         if (err) {
             console.error(err.message);
         }
-        console.log('Connected to the TP1 database.');
+       //Connected to the SQL TP1 database
     });
 
     db.serialize(function () {
         db.run("DROP TABLE IF EXISTS spells");
 
         db.run("CREATE TABLE spells (name TEXT, level INTEGER, components TEXT, spell_resistance TEXT, class TEXT)");
-
+        console.log('Table SQL created.');
 
     });
     db.close((err) => {
         if (err) {
             console.error(err.message);
         }
-        console.log('Close the database connection.');
+       //Close the SQL database connection.
     });
 
 }
@@ -65,20 +61,19 @@ function insertSpellSQL(doc) {
         if (err) {
             console.error(err.message);
         }
-        console.log('Connected to the TP1 database.');
     });
     //avoid sqlite_busy error
     db.configure("busyTimeout", 30000);
 
     
             db.run(`INSERT INTO spells VALUES (?,?,?,?,?)`,[doc.name,doc.level, doc.components,doc.spell_resistance,doc.class]); 
+            console.log('Inserted SQL');
 
 
         db.close((err) => {
             if (err) {
                 console.error(err.message);
             }
-            console.log('Close the database connection.');
         });
 
 }
